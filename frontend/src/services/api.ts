93185677067a8b97f92ebbd69d083f59324592d4
Noel Mathews <106422418+Noel9812@users.noreply.github.com>
2026-04-23 01:56:27 +0530

    initial

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://44.213.149.30:3000",
+  baseURL: window.location.origin.replace(":5173", ":3000"),
 });
 
 API.interceptors.request.use((req) => {
